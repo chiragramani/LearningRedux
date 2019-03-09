9755564474c7084bc82f94f01935221a5dbbb4a8
Chirag Ramani <chirag.ramani7@gmail.com>
2019-03-09 17:56:46 +0530

    Connect our custom redux store to html

--- index.js
+++ index.js
@@ -39,13 +39,32 @@ function addTodoAction(todo) {
   return {
     type: ADD_TODO,
     todo
-  }
+  };
 }
-function removeTodoAction(todo) {
-  return  {
+function addTodoToggleAction(id) {
+  return {
+    type: TOGGLE_TODO,
+    id
+  };
+}
+function addGoalAction(goal) {
+  return {
+    type: ADD_GOAL,
+    goal
+  };
+}
+function removeGoalAction(id) {
+  return {
+    type: REMOVE_GOAL,
+    id
+  };
+}
+
+function removeTodoAction(id) {
+  return {
     type: REMOVE_TODO,
     id
-  }
+  };
 }
 
 //// Reducer functions are specific to business logic, therefore App's code.
@@ -54,10 +73,10 @@ function todos(state = [], action) {
     case ADD_TODO:
       return state.concat([action.todo]);
     case REMOVE_TODO:
-      return state.filter(todo => todo.id !== action.todo.id);
+      return state.filter(todo => todo.id !== action.id);
     case TOGGLE_TODO:
       return state.map(todo =>
-        todo.id !== action.todo.id
+        todo.id !== action.id
           ? todo
           : Object.assign({}, todo, { complete: !todo.complete })
       );
@@ -71,7 +90,7 @@ function goals(state = [], action) {
     case ADD_GOAL:
       return state.concat([action.goal]);
     case REMOVE_GOAL:
-      return state.filter(aGoal => aGoal.id !== action.goal.id);
+      return state.filter(aGoal => aGoal.id !== action.id);
     default:
       return state;
   }
@@ -89,5 +108,97 @@ function app(state = {}, action) {
 const store = createStore(app);
 
 const unsubscribe = store.subscribe(() => {
-  console.log("The new state is", store.getState());
+  document.getElementsByClassName("todos")[0].innerHTML = '';
+  document.getElementsByClassName("goals")[0].innerHTML = '';
+  // 
+  const { todos, goals } = store.getState();
+  todos.forEach(todo => addTodoToDOM(todo));
+  goals.forEach(goal => addGoalToDOM(goal));
+});
+
+function generateId() {
+  return (
+    Math.random()
+      .toString(36)
+      .substring(2) + new Date().getTime().toString(36)
+  );
+}
+
+function createRemoveButton(onClick) {
+  const removeButton = document.createElement('button');
+  removeButton.innerHTML = 'X'
+  removeButton.addEventListener('click', onClick)
+  return removeButton
+}
+
+function addTodoToDOM(todo) {
+  const todosList = document.getElementsByClassName("todos")[0];
+  const node = document.createElement("li");
+  const text = document.createTextNode(todo.name);
+  node.appendChild(text)
+  const removeButton = createRemoveButton(() => {
+    store.dispatch(removeTodoAction(todo.id))
+  })
+  node.appendChild(removeButton)
+  node.style.textDecoration = todo.complete ? 'line-through' : 'none';
+
+  todosList.appendChild(node);
+  text.addEventListener('click', () => {
+    store.dispatch(addTodoToggleAction(todo.id))
+  })
+}
+
+function addGoalToDOM(goal) {
+  /// Adding the list element
+  const goalList = document.getElementsByClassName("goals")[0];
+  const newGoalListElement = document.createElement("li");
+  newGoalListElement.textContent = goal.name;
+  /// Remove button
+  const removeButton = createRemoveButton(() => {
+    store.dispatch(removeGoalAction(goal.id))
+  })
+  newGoalListElement.appendChild(removeButton)
+  goalList.appendChild(newGoalListElement);
+}
+
+function addTodo() {
+  const todoInput = document.getElementById("todo");
+  const currentText = todoInput.value;
+  todoInput.value = "";
+  const todo = {
+    id: generateId(),
+    name: currentText,
+    complete: false
+  };
+  store.dispatch(addTodoAction(todo));
+}
+
+function addGoal() {
+  const goalInput = document.getElementById("goal");
+  const currentText = goalInput.value;
+  goalInput.value = "";
+  const goal = {
+    id: generateId(),
+    name: currentText
+  };
+  store.dispatch(addGoalAction(goal));
+}
+
+function listenToTodoClick() {
+  const addTodoButton = document.getElementById("todoBtn");
+  addTodoButton.addEventListener("click", () => {
+    addTodo();
+  });
+}
+
+function listenToGoalClick() {
+  const addGoalButton = document.getElementById("goalBtn");
+  addGoalButton.addEventListener("click", () => {
+    addGoal();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  this.listenToTodoClick();
+  this.listenToGoalClick();
 });
